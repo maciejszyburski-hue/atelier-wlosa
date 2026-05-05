209fa23f0cb7db2cbe3cdb2d26a3278cbbd5db73
Maciej Szyburski <maciej.szyburski@gmail.com>
2026-05-05 10:42:31 +0200

    Build and route gallery page

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        galeria: resolve(__dirname, 'galeria.html'),
+      },
+    },
+  },
   preview: {
     host: '0.0.0.0',
     port: parseInt(process.env.PORT) || 4173,
